e968b0ebc80864e56b1fccef005696cdbbcd6bdb
awenirk <avenuslistioad@gmail.com>
2026-09-05 19:45:07 +0300

    commit

--- 04_03_HomeWork/04_03_HomeWork.cpp
+++ 04_03_HomeWork/04_03_HomeWork.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // 1.Вивести на екран цілі числа від 14 до 123. - while
-    int temp, count, N, dob, sum;
+    int temp, count, N, dob, sum, num1, num2;
     double aref;
     /*
     temp = 14;
@@ -110,7 +110,15 @@ int main()
 
     // 8. Вивести на екран усі числа від нуля до вве-
     // деного користувачем числа.
-    
+    /*
+    cout << "Enter num: "; cin >> temp;
+    N = 0;
+    while (N <= temp) {
+        cout << N << " ";
+        N++;
+    }
+    */
+
     // 9. Користувач вводить дві межі діапазону, ви-
     // вести на екран усі числа з цього діапазону. Передбачити,
     // щоб користувач міг вводити межі діапазону в довільному
@@ -118,17 +126,67 @@ int main()
     // ■ вивести всі парні числа з діапазону.
     // ■ вивести всі непарні числа з діапазону.
     // ■ вивести всі числа, кратні семи.
-
-
+    /*
+    cout << "Enter num1: "; cin >> num1;
+    cout << "Enter num2: "; cin >> num2;
+    if (num1 > num2) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    else if (num1 == num2){
+        cout << "Numbers are equal!" << endl;
+    }
+    N = num1;
+    while (num1 <= num2) {
+        if (num1 % 2 == 0) {
+            cout<< num1 <<" ";
+        }
+        num1++;
+    }
+    num1 = N;
+    cout << endl;
+    while (num1 <= num2) {
+        if (num1 % 2 != 0) {
+            cout<< num1 <<" ";
+        }
+        num1++;
+    }
+    num1 = N;
+    cout << endl;
+    while (num1 <= num2) {
+        if (num1 % 7 == 0) {
+            cout<< num1 <<" ";
+        }
+        num1++;
+    }
+    */
 
     // 10. Користувач вводить дві межі діапазону. По-
     // рахувати суму всіх чисел діапазону.
-
-
+    /*
+    cout << "Enter num1: "; cin >> num1;
+    cout << "Enter num2: "; cin >> num2;
+    sum = 0;
+    while (num1 <= num2) {
+        sum += num1;
+        num1++;
+    }
+    cout << "Sum: " << sum << endl;
+    */
 
     // 11. Користувач з клавіатури вводить числа. По-
     // рахувати їхню суму і вивести на екран, щойно користувач
     // введе нуль.
+    /*
+    temp = 1;
+    sum = 0;
+    cout << "Enter 0 to exit." << endl;
+    while (temp != 0) {
+        cout << "Enter num: "; cin >> temp;
+        sum += temp;
+    }
+    cout << "Sum: " << sum << endl;
+    */
 
-    
 }
