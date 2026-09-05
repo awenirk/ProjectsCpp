eae575e4dc85e4aebd2202c1276323985a1b6afa
awenirk <avenuslistioad@gmail.com>
2026-09-05 16:02:28 +0300

    Complete all 11 Tasks

--- 03_03_HomeWork'/03_03_HomeWork.cpp
+++ 03_03_HomeWork'/03_03_HomeWork.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// 03_03 task 1 complete
 
 int main()
 {
 	//1.Дано сторони прямокутника.Визначити чи прямокутник є квадратом.
+	/*
 	cout << "\tTask 1" << endl;
 	int a, b;
 	cout << "Enter b: "; cin >> b;
@@ -16,10 +16,12 @@ int main()
 	else {
 		cout << "No" << endl;
 	}
+	*/
 
 
 	// 2.Дано вік дитини.Виначити чи дитина по віку може
 	// навчатися у школі, чи може відвідувати дитсадок і т.і.
+	/*
 	cout << "\tTask 2" << endl;
 	int age;
 	cout << "Enter child`s age: "; cin >> age;
@@ -33,10 +35,12 @@ int main()
 		cout << "The child is already an adult!";
 	}
 	else {
-		cout<<"The child is too young!";
+		cout << "The child is too young!";
 	}
+	*/
 
 	// 3.Дано 3 числа.Визначити чи рівно два серед них від’ємні.
+	/*
 	cout << "\tTask 3" << endl;
 	int num1, num2, num3;
 	cout << "Enter num 1: "; cin >> num1;
@@ -48,8 +52,10 @@ int main()
 	{
 		cout << "2 nums is negative!" << endl;
 	}
+	*/
 
 	// 4.Дано число.Визначити чи належить воно відрізку[2, 5] або[-1, 1].
+	/*
 	cout << "\tTask 4" << endl;
 	int someNum;
 	cout << "Enter some num: "; cin >> someNum;
@@ -61,8 +67,10 @@ int main()
 	{
 		cout << "In second range!";
 	}
+	*/
 
 	// 5.Дано три числа. Визначити чи тільки два з них рівні між собою.
+	/*
 	cout << "\tTask 5" << endl;
 	int num4, num5, num6;
 	cout << "Enter num 1: "; cin >> num4;
@@ -74,8 +82,10 @@ int main()
 	{
 		cout << "Yes!" << endl;
 	}
+	*/
 
 	// 6.Дано 3 числа.Визначити чи всі числа непарні.Числа вводяться з клавіатури
+	/*
 	cout << "\tTask 6" << endl;
 	int num7, num8, num9;
 	cout << "Enter num 1: "; cin >> num7;
@@ -85,12 +95,14 @@ int main()
 	{
 		cout << "Every number odd!";
 	}
+	*/
 
 
 
 	//На додаткові 12 балів(за бажанням) :
 
 	//	7.Дано два числа.Якщо вони різні, то поміняти іх місцями.
+	/*
 	cout << "\tTask 7" << endl;
 	int num10, num11, temp;
 	cout << "Enter num 1: "; cin >> num10;
@@ -102,10 +114,12 @@ int main()
 	}
 	cout << "Num 1: " << num10 << endl;
 	cout << "Num 2: " << num11 << endl;
+	*/
 
 	// 8.Дано натуральне число а(а > 100 && a < 999).Напишіть програму, що визначає кількість цифр в цьому числі,
 	//	суму його цифр, і яка виводить на екран першу и останню цифру через два пропуски.
 	//	777 = 3 числа, сума, сума = 21
+	/*
 	cout << "\tTask 8" << endl;
 	int NumA;
 	cout << "Enter numA (>100 & <999): "; cin >> NumA;
@@ -116,31 +130,77 @@ int main()
 	cout << "Number of digits: 3" << endl;
 	cout << "Sum: " << sum << endl;
 	cout << first << "  " << last << endl;
+	*/
 
 	// 9.Дано час(години, хвилини, секунди).Визначити чи час допустимий(наприклад, 25.61.60 – недопустимий).
+	/*
 	cout << "\tTask 9" << endl;
-
-
-
-
-
+	int sec, min, hour;
+	cout << "Enter hours: " << endl; cin >> hour;
+	cout << "Enter minutes: " << endl; cin >> min;
+	cout << "Enter seconds: " << endl; cin >> sec;
+	if (hour > 24 || hour < 0) {
+		cout << "Invalid hours!";
+	}
+	else if (min > 60 || min < 0) {
+		cout << "Invalid minutes!";
+	}
+	else if (sec > 60 || sec < 0) {
+		cout << "Invalid seconds!";
+	}
+	else {
+		cout << "Good time!" << endl;
+	}
+	*/
 
 
 	// 10.Написати програму яка в залежності від введеної години виводить : good night, good day, good evening, good morning.
+	/*
 	cout << "\tTask 10" << endl;
-
-
-
-
-
+	int Hours;
+	cout << "Enter Hours: "; cin >> Hours;
+	if (Hours >= 0 && Hours < 6) {
+		cout << "Good Night!";
+	}
+	else if (Hours >= 6 && Hours < 12) {
+		cout << "Good morning!";
+	}
+	else if (Hours >= 12 && Hours < 18) {
+		cout << "Good day!";
+	}
+	else if (Hours >= 18 && Hours < 24) {
+		cout << "Good evening!";
+	}
+	*/
 
 
 	// 11.Написати програму, яка обчислює та виводить мінімум з трьох введених користувачем чисел
+	/*
 	cout << "\tTask 11" << endl;
-
-
-
-
-
-
+	int nums1, nums2, nums3;
+	cout << "Enter 1 num: "; cin >> nums1;
+	cout << "Enter 2 num: "; cin >> nums2;
+	cout << "Enter 3 num: "; cin >> nums3;
+	if (nums1 < nums2 && nums1 < nums3) {
+		cout << "The smallest number is --> " << nums1 << endl;
+	}
+	else if (nums2 < nums1 && nums2 < nums3) {
+		cout << "The smallest number is --> " << nums2 << endl;
+	}
+	else if (nums3 < nums1 && nums3 < num2) {
+		cout << "The smallest number is --> " << nums3 << endl;
+	}
+	else if (nums1 == nums2 && nums1 < nums3) {
+		cout << "The smallest number is --> " << nums1, nums2 << endl;
+	}
+	else if (nums2 == nums3 && nums2 < nums1) {
+		cout << "The smallest number is --> " << nums2, nums3 << endl;
+	}
+	else if (nums3 == nums1 && nums3 < nums2) {
+		cout << "The smallest number is --> " << nums1, nums3 << endl;
+	}
+	else {
+		cout << "Some nums are equals" << endl;
+	}
+	*/
 }
