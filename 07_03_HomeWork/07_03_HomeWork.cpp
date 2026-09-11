5383b925a79f0624e4644eaf9dfe8f47bf0c75d3
awenirk <avenuslistioad@gmail.com>
2026-09-12 00:29:12 +0300

    complete all 10 tasks

--- 07_03_HomeWork/07_03_HomeWork.cpp
+++ 07_03_HomeWork/07_03_HomeWork.cpp
@@ -135,111 +135,189 @@ int main()
     cout << "Elements filled: " << setw(3) << elf4 << " | 121" << endl;
 
 
-
-    /*
-    cout << endl << "Pattern 5:" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (i <= j && i + j < N or i >= j && i + j >= N - 1)
-            {
-                cout << "* ";
+    cout << endl << "\tPattern 5:" << endl; // i <= j && i + j < N or i >= j && i + j >= N - 1
+    cout << "==================================#" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i <= j && i + j < N or i >= j && i + j >= N - 1) {
+                arr5[i][j] = 1 + rand() % 64;
+                elf5++;
             }
-            else
-            {
-                cout << "  ";
+            else arr5[i][j] = 0;
+
+            cout << setw(3) << arr5[i][j];
+
+            if (i == 0 && j == 0) {
+                min5 = arr5[i][j];
+                max5 = arr5[i][j];
+            }
+            else {
+                if (arr5[i][j] < min5 && i <= j && i + j < N or i >= j && i + j >= N - 1) min5 = arr5[i][j];
+                if (arr5[i][j] > max5 && i <= j && i + j < N or i >= j && i + j >= N - 1) max5 = arr5[i][j];
             }
         }
-        cout << "|" << endl;
+        cout << " |" << endl;
     }
+    cout << "==================================#" << endl;
+    cout << "Min element in 5 pattern: " << setw(3) << min5 << " | 64" << endl;
+    cout << "Max element in 5 pattern: " << setw(3) << max5 << " | 64" << endl;
+    cout << "Elements filled: " << setw(3) << elf5 << " | 121" << endl;
 
-    cout << endl << "Pattern 6:" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (i >= j && i + j < N || i <= j && i + j >= N - 1)
-            {
-                cout << "* ";
+
+    cout << endl << "\tPattern 6:" << endl; // j && i + j < N || i <= j && i + j >= N - 1
+    cout << "==================================#" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (j && i + j < N || i <= j && i + j >= N - 1) {
+                arr6[i][j] = 1 + rand() % 64;
+                elf6++;
             }
-            else
-            {
-                cout << "  ";
+            else arr6[i][j] = 0;
+
+            cout << setw(3) << arr6[i][j];
+
+            if (i == 0 && j == 0) {
+                min6 = arr6[i][j];
+                max6 = arr6[i][j];
+            }
+            else {
+                if (arr6[i][j] < min6 && j && i + j < N || i <= j && i + j >= N - 1) min6 = arr6[i][j];
+                if (arr6[i][j] > max6 && j && i + j < N || i <= j && i + j >= N - 1) max6 = arr6[i][j];
             }
         }
-        cout << "|" << endl;
+        cout << " |" << endl;
     }
+    cout << "==================================#" << endl;
+    cout << "Min element in 6 pattern: " << setw(3) << min6 << " | 64" << endl;
+    cout << "Max element in 6 pattern: " << setw(3) << max6 << " | 64" << endl;
+    cout << "Elements filled: " << setw(3) << elf6 << " | 121" << endl;
 
-    cout << endl << "Pattern 7:" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (i >= j && i + j < N)
-            {
-                cout << "* ";
+
+    cout << endl << "\tPattern 7:" << endl; // i >= j && i + j >= N - 1
+    cout << "==================================#" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i >= j && i + j < N) {
+                arr7[i][j] = 1 + rand() % 64;
+                elf7++;
             }
-            else
-            {
-                cout << "  ";
+            else arr7[i][j] = 0;
+
+            cout << setw(3) << arr7[i][j];
+
+            if (i == 0 && j == 0) {
+                min7 = arr7[i][j];
+                max7 = arr7[i][j];
+            }
+            else {
+                if (arr7[i][j] < min7 && i >= j && i + j < N) min7 = arr7[i][j];
+                if (arr7[i][j] > max7 && i >= j && i + j < N) max7 = arr7[i][j];
             }
         }
-        cout << "|" << endl;
+        cout << " |" << endl;
     }
+    cout << "==================================#" << endl;
+    cout << "Min element in 7 pattern: " << setw(3) << min7 << " | 64" << endl;
+    cout << "Max element in 7 pattern: " << setw(3) << max7 << " | 64" << endl;
+    cout << "Elements filled: " << setw(3) << elf7 << " | 121" << endl;
 
 
-    cout << endl << "Pattern 8:" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (i <= j && i + j >= N - 1)
-            {
-                cout << "* ";
+    cout << endl << "\tPattern 8:" << endl; // i >= j && i + j >= N - 1
+    cout << "==================================#" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i <= j && i + j >= N - 1) {
+                arr8[i][j] = 1 + rand() % 64;
+                elf8++;
             }
-            else
-            {
-                cout << "  ";
+            else arr8[i][j] = 0;
+
+            cout << setw(3) << arr8[i][j];
+
+            if (i == 0 && j == 0) {
+                min8 = arr8[i][j];
+                max8 = arr8[i][j];
+            }
+            else {
+                if (arr8[i][j] < min8 && i <= j && i + j >= N - 1) min8 = arr8[i][j];
+                if (arr8[i][j] > max8 && i <= j && i + j >= N - 1) max8 = arr8[i][j];
             }
         }
-        cout << "|" << endl;
+        cout << " |" << endl;
     }
+    cout << "==================================#" << endl;
+    cout << "Min element in 8 pattern: " << setw(3) << min8 << " | 64" << endl;
+    cout << "Max element in 8 pattern: " << setw(3) << max8 << " | 64" << endl;
+    cout << "Elements filled: " << setw(3) << elf8 << " | 121" << endl;
 
-    cout << endl << "Pattern 9:" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (i + j <= N - 1)
-            {
-                cout << "* ";
+
+    cout << endl << "\tPattern 9:" << endl; // i >= j && i + j >= N - 1
+    cout << "==================================#" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i + j <= N - 1) {
+                arr9[i][j] = 1 + rand() % 64;
+                elf9++;
             }
-            else
-            {
-                cout << "  ";
+            else arr9[i][j] = 0;
+
+            cout << setw(3) << arr9[i][j];
+
+            if (i == 0 && j == 0) {
+                min9 = arr9[i][j];
+                max9 = arr9[i][j];
+            }
+            else {
+                if (arr9[i][j] < min9 && i + j <= N - 1) min9 = arr9[i][j];
+                if (arr9[i][j] > max9 && i + j <= N - 1) max9 = arr9[i][j];
             }
         }
-        cout << "|" << endl;
+        cout << " |" << endl;
     }
+    cout << "==================================#" << endl;
+    cout << "Min element in 9 pattern: " << setw(3) << min9 << " | 64" << endl;
+    cout << "Max element in 9 pattern: " << setw(3) << max9 << " | 64" << endl;
+    cout << "Elements filled: " << setw(3) << elf9 << " | 121" << endl;
 
-    cout << endl << "Pattern 10:" << endl;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            if (i + j > N)
-            {
-                cout << "* ";
+
+    cout << endl << "\tPattern 0:" << endl; // i >= j && i + j >= N - 1
+    cout << "==================================#" << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i + j > N) {
+                arr0[i][j] = 1 + rand() % 64;
+                elf0++;
             }
-            else
-            {
-                cout << "  ";
+            else arr0[i][j] = 0;
+
+            cout << setw(3) << arr0[i][j];
+
+            if (i == 0 && j == 0) {
+                min0 = arr0[i][j];
+                max0 = arr0[i][j];
+            }
+            else {
+                if (arr0[i][j] < min0 && i + j > N) min0 = arr0[i][j];
+                if (arr0[i][j] > max0 && i + j > N) max0 = arr0[i][j];
             }
         }
-        cout << "|" << endl;
+        cout << " |" << endl;
     }
-    */
+    cout << "==================================#" << endl;
+    cout << "Min element in 0 pattern: " << setw(3) << min0 << " | 64" << endl;
+    cout << "Max element in 0 pattern: " << setw(3) << max0 << " | 64" << endl;
+    cout << "Elements filled: " << setw(3) << elf0 << " | 121" << endl;
 
+
+    // 1 --> i <= j
+    // 2 --> i >= j
+    // 3 --> i <= j && i + j < N
+    // 4 --> i >= j && i + j >= N - 1
+    // 5 --> i <= j && i + j < N or i >= j && i + j >= N - 1
+    // 6 --> i >= j && i + j < N || i <= j && i + j >= N - 1
+    // 7 --> i >= j && i + j < N
+    // 8 --> i <= j && i + j >= N - 1
+    // 9 --> i + j <= N - 1
+    // 0 --> i + j > N
 
 }
