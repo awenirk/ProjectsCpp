184625ef9311daaee67841bea1702ffee096463c
Awenirk <avenuslistopad@gmail.com>
2026-09-11 17:33:33 +0300

    little changes in 07_02, complete task 2

--- 07_01_ClassWork/07_01_ClassWork.cpp
+++ 07_01_ClassWork/07_01_ClassWork.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <iomanip>
+#include <clocale>
 using namespace std;
 
 int main()
 {
     int a, max, min;
     srand(time(0));
+    setlocale(LC_ALL, "Ukrainian");
 
     /*
     for (int i = 0; i < 50; i++)
