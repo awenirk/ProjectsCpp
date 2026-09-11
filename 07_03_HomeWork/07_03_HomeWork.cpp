e6f87ba229b4f6d8c85508d0edd99fd896da52e4
awenirk <avenuslistioad@gmail.com>
2026-09-11 22:14:17 +0300

    added basic 07_03_HomeWork

---
+++ 07_03_HomeWork/07_03_HomeWork.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    srand(time(0));
+    int a, b, max, min;
+
+}
