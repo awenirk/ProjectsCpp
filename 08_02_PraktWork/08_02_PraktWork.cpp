76d58a5aff67a4371de14235785ce3f2c3e67c5a
awenirk <avenuslistioad@gmail.com>
2026-09-15 12:09:21 +0300

    completed 2, 3 task

--- 08_02_PraktWork/08_02_PraktWork.cpp
+++ 08_02_PraktWork/08_02_PraktWork.cpp
@@ -5,19 +5,32 @@ using namespace std;
 
 
 void drawSquare(int N, int K);
-int factorial(int num);
+long long factorial(long long num);
+bool isPrime(int num);
+
+
+
+
+
 
 int main()
 {
     setlocale(LC_ALL, "Ukrainian");
     srand(time(0));
-    int a = 60;
+    long long someNum = 60;
 
     drawSquare(4, 10);
-    cout << "Enter factorial: "; cin >> a;
-    cout << factorial(a) << endl;
+
+    cout << "Enter factorial (< 21): "; cin >> someNum;
+    cout << someNum << "! = " << factorial(someNum) << endl;
+
+    cout << "Enter number: "; cin >> someNum;
+    cout << isPrime(someNum) << endl;
+
 
 }
+
+
 //1.Написати функцію, що виводить на екран прямокут-
 // ник з висотою N і шириною K.
 void drawSquare(int N, int K) {
@@ -30,25 +43,57 @@ void drawSquare(int N, int K) {
 }
 // 2. Написати функцію, яка обчислює факторіал переда -
 // ного їй числа. 5!= 1 * 2 * 3 * 4 * 5
-int factorial(int num) {
-    int num1 = 1;
+long long factorial(long long num) {
+    long long num1 = 1;
     for (int i = 1; i <= num; i++) {
         num1 *= i;
     }
     return num1;
 }
-
-
-
 // 3 * **.Написати функцію, яка перевіряє, чи є передане їй
 // число простим.Число називається простим, якщо воно
 // ділиться без залишку тільки на себе та на одиницю.
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+// 4. Написати функцію, яка повертає куб числа.
 
 
 
 
-// 4. Написати функцію, що визначає мінімум і максимум
-// (значення й номер) елементів переданого їй масиву.
+// 5. Написати функцію для знаходження найбільшого з двох чисел.
+
+
+
+
+// 6. Написати функцію, яка повертає істину, якщо передане значення додатне, та брехню, якщо від’ємне.
+
+
+
+
+
+// На додаткові 12 балів:
+// 1. Написати функцію, що визначає мінімум і максимум
+// (значення й номер) елементів переданого їй масиву. 
+
+
+
+// 2. Написати функцію, яка міняє порядок елементів
+// переданого їй масиву на протилежний.
+
+
+
+
+// 3. Написати функцію, яка повертає кількість
+// простих чисел у переданому їй масиві.
+
+
 
 
 
